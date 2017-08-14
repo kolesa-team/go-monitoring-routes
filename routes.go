874b3b0022b6456cb4bd539a5fdf04f082f464a9
Igor Borodikhin <iborodikhin@gmail.com>
2017-08-14 10:04:53 +0600

    /version route

--- routes.go
+++ routes.go
@@ -11,7 +11,12 @@ import (
 	"github.com/zenazn/goji/web"
 )
 
-func Router(m *web.Mux, cn *c.Config, l *logrus.Logger, s func() map[interface{}]interface{}) {
+func Router(m *web.Mux, v interface{}, cn *c.Config, l *logrus.Logger, s func() map[interface{}]interface{}) {
+	// Handle /version route
+	m.Get("/version", func(c web.C, w http.ResponseWriter, r *http.Request) {
+		http.Error(w, fmt.Sprintf("%v", v), http.StatusOK)
+	})
+
 	// Handle /config route
 	m.Get("/config", func(c web.C, w http.ResponseWriter, r *http.Request) {
 		http.Error(w, config.Dump(cn), http.StatusOK)
