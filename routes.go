3319dd2d59b4305ac5c1d02632c813a2299972ef
Igor Borodikhin <iborodikhin@gmail.com>
2017-08-15 08:43:53 +0600

    refactor

--- routes.go
+++ routes.go
@@ -11,32 +11,68 @@ import (
 	"github.com/zenazn/goji/web"
 )
 
-func Router(m *web.Mux, v interface{}, cn *c.Config, l *logrus.Logger, s func() map[string]interface{}) {
-	// Handle /version route
-	m.Get("/version", func(c web.C, w http.ResponseWriter, r *http.Request) {
-		http.Error(w, fmt.Sprintf("%v", v), http.StatusOK)
+type Router struct {
+	mux        *web.Mux
+	conf       *c.Config
+	log        *logrus.Logger
+	version    interface{}
+	statusFunc func() map[string]interface{}
+	healthFunc func() bool
+}
+
+func NewRouter() *Router {
+	r := Router{}
+
+	return &r
+}
+
+// Set mux for router
+func (r *Router) Mux(m *Web.Mux) *Router {
+	r.mux = m
+
+	return r
+}
+
+// Set /version route handler
+func (r *Router) Version(v interface{}) *Router {
+	r.version = v
+
+	r.mux.Get("/version", func(c web.C, w http.ResponseWriter, req *http.Request) {
+		http.Error(w, fmt.Sprintf("%v", r.version), http.StatusOK)
 	})
 
-	// Handle /config route
-	m.Get("/config", func(c web.C, w http.ResponseWriter, r *http.Request) {
-		http.Error(w, config.Dump(cn), http.StatusOK)
+	return r
+}
+
+// Set /config route handler
+func (r *Router) Config(cn *c.Config) *Router {
+	r.conf = cn
+
+	r.mux.Get("/config", func(c web.C, w http.ResponseWriter, req *http.Request) {
+		http.Error(w, config.Dump(r.conf), http.StatusOK)
 	})
 
-	// Handle /debug routes
-	m.Post("/debug", func(c web.C, w http.ResponseWriter, r *http.Request) {
-		if l.Level != logrus.DebugLevel {
-			l.Level = logrus.DebugLevel
-			l.Debug("Debug level set")
+	return r
+}
+
+// Set /debug route handler
+func (r *Router) Logger(l *logrus.Logger) *Router {
+	r.log = l
+
+	r.mux.Post("/debug", func(c web.C, w http.ResponseWriter, req *http.Request) {
+		if r.log.Level != logrus.DebugLevel {
+			r.log.Level = logrus.DebugLevel
+			r.log.Debug("Debug level set")
 
 			http.Error(w, "ok", http.StatusOK)
 		} else {
 			http.Error(w, "error", http.StatusOK)
 		}
 	})
-	m.Delete("/debug", func(c web.C, w http.ResponseWriter, r *http.Request) {
-		if l.Level == logrus.DebugLevel {
-			l.Level = logrus.InfoLevel
-			l.Info("Debug level unset")
+	r.mux.Delete("/debug", func(c web.C, w http.ResponseWriter, req *http.Request) {
+		if r.log.Level == logrus.DebugLevel {
+			r.log.Level = logrus.InfoLevel
+			r.log.Info("Debug level unset")
 
 			http.Error(w, "ok", http.StatusOK)
 		} else {
@@ -44,13 +80,36 @@ func Router(m *web.Mux, v interface{}, cn *c.Config, l *logrus.Logger, s func() 
 		}
 	})
 
-	// Handle /status route
-	m.Handle("/status", func(c web.C, w http.ResponseWriter, r *http.Request) {
-		b, err := json.Marshal(s())
+	return r
+}
+
+// Set /status route handler
+func (r *Router) StatusFunc(f func() map[string]interface{}) *Router {
+	r.statusFunc = f
+
+	r.mux.Handle("/status", func(c web.C, w http.ResponseWriter, req *http.Request) {
+		b, err := json.Marshal(r.statusFunc())
 		if err != nil {
 			http.Error(w, fmt.Sprintf("Error marshaling status to json: %v", err), http.StatusOK)
 		} else {
 			http.Error(w, string(b), http.StatusOK)
 		}
 	})
+
+	return r
+}
+
+// Set /health route handler
+func (r *Router) HealthFunc(f func() bool) *Router {
+	r.healthFunc = f
+
+	r.mux.Handle("/health", func(c web.C, w http.ResponseWriter, req *http.Request) {
+		if r.healthFunc() {
+			http.Error(w, "green", http.StatusOK)
+		} else {
+			http.Error(w, "red", http.StatusServiceUnavailable)
+		}
+	})
+
+	return r
 }
